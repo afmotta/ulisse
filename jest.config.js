8f0718503749ae1ead6d82210423b88117e74a3f
Alberto Motta <amotta@digitalidentity.tech>
2019-06-23 21:46:56 +0200

    feat(root): initial commit

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  clearMocks: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'clover'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  globals: {
+    'ts-jest': {
+      extends: './babel.config.js',
+    },
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js'],
+  moduleNameMapper: {
+    '@taxi/(.+)$': '<rootDir>packages/$1/src',
+  },
+  modulePathIgnorePatterns: ['dist'],
+  notify: true,
+  notifyMode: 'always',
+  roots: ['<rootDir>packages'],
+  setupTestFrameworkScriptFile: '<rootDir>jest/setupTests.ts',
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  testMatch: ['**/__tests__/*.+(ts|tsx|js)', '**/*.test.+(ts|tsx|js)'],
+  testPathIgnorePatterns: ['packages/ui'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+};
---
+++ packages/ui/src/App.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  public render() {
+    const onClick = () => alert('logIn clicked');
+    return <div className="App">App</div>;
+  }
+}
+
+export default App;
