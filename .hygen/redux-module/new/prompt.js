927db2c9681f54a91c4680ad71aa81961cc96487
Alberto Francesco Motta <alberto.motta.86@gmail.com>
2019-07-11 10:44:49 +0000

    Merged PR 2: hygen generator
    feat(code generator): add hygen and redux module generator
    
    To use the generator, use the following command:
    `yarn new:redux`

---
+++ .hygen/redux-module/new/prompt.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    type: 'input',
+    name: 'scope',
+    message: 'The module scope (ie. auth)',
+  },
+  {
+    type: 'input',
+    name: 'name',
+    message: 'The module name',
+  },
+];
